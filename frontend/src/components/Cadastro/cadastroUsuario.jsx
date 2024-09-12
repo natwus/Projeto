1748d61d48da040aa582aa428d599418ff4c17bc
mareuusenai <mareussenai@gmail.com>
2024-09-12 14:25:05 -0400

    feedback de senha

--- frontend/src/components/Cadastro/cadastroUsuario.jsx
+++ frontend/src/components/Cadastro/cadastroUsuario.jsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getPermissoes, registerUser } from '../../services/userService';
 import { jwtDecode } from "jwt-decode";
+import zxcvbn from 'zxcvbn';
 
 function Cadastro() {
     const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
     const [nome, setNome] = useState('');
-    const navigate = useNavigate();
-    const token = localStorage.getItem('token');
+    const [senha, setSenha] = useState('');
+    const [senhaForte, setSenhaForte] = useState(false);
+    const [senhaFeedback, setSenhaFeedback] = useState('');
     const [permissoes, setPermissoes] = useState([]);
     const [permissaoSelecionada, setPermissaoSelecionada] = useState('')
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
 
     useEffect(() => {
         const fetchPermissoes = async () => {
@@ -31,20 +34,53 @@ function Cadastro() {
         emailLogado = decoded.id
     }
 
+    const possuiLetraMaiuscula = (senha) => {
+        const regexMaiuscula = /[A-Z]/;
+        return regexMaiuscula.test(senha);
+    };
+
+    const possuiCaractereEspecial = (senha) => {
+        const regexEspecial = /[^a-zA-Z0-9]/;
+        return regexEspecial.test(senha);
+    };
+
+    const validarSenha = (senha) => {
+        const result = zxcvbn(senha);
+        let feedback = '';
+
+        if (senha.length < 8) {
+            feedback = 'A senha deve ter pelo menos 8 caracteres.';
+        } else if (!possuiLetraMaiuscula(senha)) {
+            feedback = 'A senha deve conter pelo menos uma letra maiúscula.';
+        } else if (!possuiCaractereEspecial(senha)) {
+            feedback = 'A senha deve conter pelo menos um caractere especial.';
+        } else if (result.score >= 3) {
+            feedback = 'Senha forte!';
+        }
+
+        // Verifica a força da senha, se possui uma letra maiúscula e um caractere especial
+        setSenhaForte(result.score >= 3 && possuiLetraMaiuscula(senha) && possuiCaractereEspecial(senha));
+        setSenhaFeedback(feedback);
+    };
+
     const enviarDados = async (event) => {
         event.preventDefault();
 
-        try {
-            const data = await registerUser(nome, email, senha, permissaoSelecionada, emailLogado);
+        if (senhaForte) {
+            try {
+                const data = await registerUser(nome, email, senha, permissaoSelecionada, emailLogado);
 
-            if (data.sucess) {
-                alert('Cadastro realizado!')
-                navigate('/usuarios')
-            } else {
-                alert(data.message)
+                if (data.sucess) {
+                    alert('Cadastro realizado!');
+                    navigate('/usuarios');
+                } else {
+                    alert(data.message);
+                }
+            } catch (error) {
+                console.error('Erro ao enviar os dados:', error);
             }
-        } catch (error) {
-            console.error('Erro ao enviar os dados:', error);
+        } else {
+            alert('A senha deve ser forte!');
         }
     };
 
@@ -69,10 +105,21 @@ function Cadastro() {
                 <label>Senha</label>
                 <input
                     type="password"
-                    name="password"
+                    name="senha"
                     value={senha}
-                    onChange={(e) => setSenha(e.target.value)}
+                    onChange={(e) => {
+                        setSenha(e.target.value);
+                        validarSenha(e.target.value);
+                    }}
                 />
+                {senha && (
+                    <div>
+                        <p style={{ color: senhaForte ? 'green' : 'red' }}>
+                            {senhaFeedback}
+                        </p>
+                    </div>
+                )}
+                
                 <label>Permissão</label>
                 <select
                     name="permissao"
